460436fd4446165831b8839eb23615b582f6c1a7
Thomas Robacker <trobacker21@gmail.com>
2024-11-28 09:11:11 -0500

    Adding NA simple regression.

--- Data/basic_reg_data.R
+++ Data/basic_reg_data.R
@@ -1,4 +1,5 @@
 ## Generate simple regression data
+library(tidyverse)
 set.seed(77)
 n <- 100
 x <- round(runif(n, min = 0, max = 10),2)
@@ -8,4 +9,13 @@ eps <- round(rnorm(n, mean = 0, sd = 2), 2)
 y <- 2*x+eps
 
 df <- data.frame("x" = x, "y" = y)
-write.csv(df, file = "./simplereg_data")
+#write.csv(df, file = "./simplereg_data")
+
+# Random indices to induce NA at rate 10%
+id_remove <- rbinom(n = n, size = 1, prob = 0.10)
+df["missing"] <- id_remove
+
+df %>%
+  mutate(x_na = if_else( (missing > 0), NA_real_, x))
+
+write.csv(df, file = "./Data/simpleregwithNA_data")
